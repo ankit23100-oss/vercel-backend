fe5ef61cb1fa51bdee94987a997de897e7ab4be9
ankit-id-12 <ankit00sunbeam@gmail.com>
2026-05-15 23:37:12 +0530

    string

--- app.js
+++ app.js
@@ -94,4 +94,8 @@ io.on("connection",function(uniquesocket){
     })
 })
 
-server.listen(3000);
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log("server running");
+});
